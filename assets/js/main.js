bfa6c6f6ed3028da4a97c54457612ffca989f40a
Matteo Francesco Leuterio <leuterio.matteo@gmail.com>
2020-05-11 15:15:50 +0200

    html+css done

---
+++ assets/js/main.js
@@ -0,0 +1,42 @@
+console.log('ok');
+console.log('jQuery ->', $);
+
+/**
+ * 
+ *  TODOOLEAN
+ * 
+ */
+
+ 
+$(document).ready( function() {
+    
+    /*
+    *  SETUP
+    */
+
+    //refs
+
+
+
+    //Api
+
+
+ 
+    // Init Handlebars
+
+
+
+    /*
+    *  ACTIONS 
+    */
+
+
+
+});
+
+//FUNCTIONS
+
+
+
+
+
